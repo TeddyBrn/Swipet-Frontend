4b492e71a0cf597739474f20705287a4592521d0
Your name <Your email>
2024-05-21 10:21:03 +0200

    logo to swipescreen

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
