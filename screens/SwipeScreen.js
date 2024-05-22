837a9ab21821ec8e9004485a7cd0b38b74567f5d
Your name <Your email>
2024-05-22 15:04:22 +0200

    import Dimensions dansSwipeScreen

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { profilData } from '../data/profils';
-import { useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addLike } from '../reducers/users';
+import { profilData } from '../data/profils';
 
 export default function ProfileCard({ navigation }) {
-const dispatch = useDispatch();
-const user = useSelector((state)=> state.users.value)
-const addAlike = () => dispatch(addLike(profilData[count]._id))
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.users.value);
+  const addAlike = () => dispatch(addLike(profilData[count]._id));
 
   const [count, setCount] = useState(0);
   const [name, setName] = useState('');
@@ -24,7 +23,7 @@ const addAlike = () => dispatch(addLike(profilData[count]._id))
     setAge(profilData[0].age);
     setBio(profilData[0].bio);
     setImg(profilData[0].url);
-    setNote(profilData[count].avis[0].note);
+    setNote(profilData[0].avis[0].note);
   }, []);
 
   const handleLike = () => {
@@ -34,10 +33,7 @@ const addAlike = () => dispatch(addLike(profilData[count]._id))
     setBio(profilData[count].bio);
     setImg(profilData[count].url);
     addAlike();
-    
-
   };
-  console.log(user.like)
 
   const handleDislike = () => {
     setCount(count + 1);
@@ -46,20 +42,12 @@ const addAlike = () => dispatch(addLike(profilData[count]._id))
     setBio(profilData[count].bio);
     setImg(profilData[count].url);
     setNote(profilData[count].avis[0].note);
-    
-    
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        
-          <Image
-            source={require('../assets/miniLogo.png')}
-            style={styles.logo}
-          />
-          
-        
+        <Image source={require('../assets/miniLogo.png')} style={styles.logo} />
         <View style={styles.headerR}>
           <TouchableOpacity
             style={styles.iconButton}
@@ -77,17 +65,11 @@ const addAlike = () => dispatch(addLike(profilData[count]._id))
         <View style={styles.profileContainer}>
           <Image source={img} style={styles.profileImage} />
           <View style={styles.ratingContainer}>
-          <Text style={styles.profileNote}>
-              {note}/5
-            </Text>
+            <Text style={styles.profileNote}>{note}/5</Text>
             {Array(Math.round(note))
               .fill()
-              .map((i) => (
-                <Image
-                  key={i}
-                  source={require('../assets/star.png')}
-                  style={styles.star}
-                />
+              .map((_, i) => (
+                <Image key={i} source={require('../assets/star.png')} style={styles.star} />
               ))}
           </View>
           <View style={styles.infos}>
@@ -103,11 +85,7 @@ const addAlike = () => dispatch(addLike(profilData[count]._id))
             <Ionicons name="close-circle-outline" size={80} color="#CD4F4F" />
           </TouchableOpacity>
           <TouchableOpacity activeOpacity={0.7} onPress={handleLike}>
-            <Ionicons
-              name="checkmark-circle-outline"
-              size={80}
-              color="#8FD14F"
-            />
+            <Ionicons name="checkmark-circle-outline" size={80} color="#8FD14F" />
           </TouchableOpacity>
         </View>
       </View>
@@ -115,10 +93,13 @@ const addAlike = () => dispatch(addLike(profilData[count]._id))
   );
 }
 
+// Récupère les dimensions de l'écran
+const { width, height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   header: {
     flexDirection: 'row',
@@ -129,16 +110,16 @@ const styles = StyleSheet.create({
     shadowColor: '#000000',
     shadowOffset: {
       width: 0,
-      height: 18
+      height: 18,
     },
     shadowOpacity: 0.25,
     shadowRadius: 20.0,
-    elevation: 20
+    elevation: 20,
   },
   headerR: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingVertical: 10
+    paddingVertical: 10,
   },
   logo: {
     width: 60,
@@ -147,21 +128,19 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginVertical: 5,
   },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
   iconButton: {
-    marginHorizontal: 10
+    marginHorizontal: 10,
   },
   main: {
     width: '100%',
     height: '100%',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   profileContainer: {
-    height: '78%',
-    width: '90%',
+    // Adapter la hauteur à un pourcentage de la hauteur de l'écran
+    height: height * 0.69,
+    // Adapter la largeur à un pourcentage de la largeur de l'écran
+    width: width * 0.9,
     flexDirection: 'column',
     alignItems: 'center',
     backgroundColor: '#EAEAEA',
@@ -170,31 +149,32 @@ const styles = StyleSheet.create({
     shadowColor: '#000000',
     shadowOffset: {
       width: 0,
-      height: 18
+      height: 18,
     },
     shadowOpacity: 0.25,
     shadowRadius: 20.0,
-    elevation: 5
+    elevation: 5,
   },
   profileImage: {
+    // Adapter la hauteur à un pourcentage de la hauteur de l'écran
     width: '95%',
-    height: 340,
+    height: height * 0.4,
     borderRadius: 15,
-    resizeMode: 'cover'
+    resizeMode: 'cover',
   },
   ratingContainer: {
     flexDirection: 'row',
     marginVertical: 10,
     justifyContent: 'flex-start',
-    width: '90%'
+    width: '90%',
   },
   star: {
     width: 30,
     height: 30,
-    marginHorizontal: 1
+    marginHorizontal: 1,
   },
   infos: {
-    width: '89%'
+    width: '89%',
   },
   profileName: {
     fontSize: 25,
@@ -204,31 +184,33 @@ const styles = StyleSheet.create({
     borderTopWidth: 1.5,
     borderBottomColor: 'gray',
     borderBottomWidth: 1.5,
-    paddingLeft: 10
+    paddingLeft: 10,
   },
   profileNote: {
     fontSize: 20,
     fontWeight: 'bold',
     paddingLeft: 10,
-    paddingRight: 10
+    paddingRight: 10,
   },
   bioLabel: {
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 8,
-    marginBottom: 15
+    marginBottom: 15,
   },
   bioText: {
     fontSize: 18,
-    width: '90%'
+    width: '90%',
   },
   actionButtons: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
-    marginTop: 10
-  }
+    marginTop: 10,
+  },
 });
+
+
 
 /*
 Dans dossier assets
@@ -253,43 +235,6 @@ ou  <Tab.Screen name="SwipeScreen" component={SwipeScreen} />
 
 
 
-const profilesData = [
-  {
-    uriPhoto: '../assets/myprofile.jpg',
-    note: 4.5,
-    name: "Alice",
-    age: 28,
-    bio: "Grande amoureuse des animaux de compagnie, surtout des chats et des chiens."
-  },
-  {
-    uriPhoto: '../assets/myprofile.jpg',
-    note: 3.8,
-    name: "Bob",
-    age: 34,
-    bio: "Passionné par les animaux domestiques,  j'adore passer du temps avec mes deux chiens."
-  },
-  {
-    uriPhoto: '../assets/myprofile.jpg',
-    note: 4.9,
-    name: "Claire",
-    age: 26,
-    bio: "je trouve une grande joie à m'occuper de ses animaux de compagnie"
-  },
-  {
-    uriPhoto: '../assets/myprofile.jpg',
-    note: 4.2,
-    name: "David",
-    age: 31,
-    bio: "Les animaux de compagnie occupent une place spéciale dans sa vie, j'aime les perroquets et les poissons."
-  },
-  {
-    uriPhoto: '../assets/myprofile.jpg',
-    note: 4.7,
-    name: "Elise",
-    age: 29,
-    bio: "toujours eu une passion pour les animaux domestiques, particulièrement les lapins et les hamsters."
-  }
-];
 
 
 
