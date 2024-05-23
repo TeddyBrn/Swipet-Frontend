b359d2f522b03970fdc9fc5c29480bd1ef090025
Teddy <teddy.buran33@gmail.com>
2024-05-23 18:37:40 +0200

    design

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   View,
   Text,
@@ -22,110 +22,121 @@ export default function ProfileCard({ navigation }) {
   const addAlike = () => dispatch(addLike(profilData[count]._id));
 
   const [count, setCount] = useState(0);
-  //   const [name, setName] = useState('');
-  //   const [age, setAge] = useState('');
-  //   const [bio, setBio] = useState('');
-  //   const [img, setImg] = useState('');
-  //   const [note, setNote] = useState(0);
-  //   console.log(img)
-
-  //   useEffect(() => {
-  //     setName(profilData[0].firstname);
-  //     setAge(profilData[0].age);
-  //     setBio(profilData[0].bio);
-  //     setImg(profilData[0].url);
-  //     setNote(profilData[0].avis[0].note);
-  //   }, []);
 
   const handleLike = () => {
     setCount(count + 1);
-    // setName(profilData[count].firstname);
-    // setAge(profilData[count].age);
-    // setBio(profilData[count].bio);
-    // setImg(profilData[count].url);
-    // setNote(profilData[count].avis[0].note);
     addAlike();
   };
   console.log(`user.like => ${user.like}`);
   const handleDislike = () => {
     setCount(count + 1);
-    // setName(profilData[count].firstname);
-    // setAge(profilData[count].age);
-    // setBio(profilData[count].bio);
-    // setImg(profilData[count].url);
-    // setNote(profilData[count].avis[0].note);
+  };
+
+  const swiperRef = useRef(null);
+
+  const handleSwipeLeft = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swipeLeft();
+    }
+  };
+
+  const handleSwipeRight = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swipeRight();
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.i}>
-      <View style={styles.header}>
-        <Image source={require('../assets/miniLogo.png')} style={styles.logo} />
-        <View style={styles.headerR}>
-          <TouchableOpacity
-            style={styles.iconButton}
-            onPress={() => navigation.navigate('Notifications')}>
-            <Ionicons name="notifications" size={35} color="#33464d" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.iconButton}
-            onPress={() => navigation.navigate('Settings')}>
-            <Ionicons name="settings-outline" size={35} color="#33464d" />
-          </TouchableOpacity>
+        <View style={styles.header}>
+          <Image
+            source={require('../assets/miniLogo.png')}
+            style={styles.logo}
+          />
+          <View style={styles.headerR}>
+            <TouchableOpacity
+              style={styles.iconButton}
+              onPress={() => navigation.navigate('Notifications')}>
+              <Ionicons name="notifications" size={35} color="#33464d" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.iconButton}
+              onPress={() => navigation.navigate('Settings')}>
+              <Ionicons name="settings-outline" size={35} color="#33464d" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View style={styles.main}>
-        <Swiper
-          cards={profilData} // Les données des profils à swiper
-          renderCard={(card) => {
-            return (
-              <View style={styles.profileContainer}>
-                <Image style={styles.profileImage} source={{ uri: card.url }} />
-                <View style={styles.infos}>
-                  <Text style={styles.profileName}>
-                    {card.firstname} , {card.age} ans
-                  </Text>
-                  <View style={styles.ratingContainer}>
-                    <Text style={styles.profileCity}> {card.city}</Text>
+        <View style={styles.main}>
+          <Swiper
+            ref={swiperRef}
+            cards={profilData} // Les données des profils à swiper
+            renderCard={(card) => {
+              return (
+                <View style={styles.profileContainer}>
+                  <Image
+                    style={styles.profileImage}
+                    source={{ uri: card.url }}
+                  />
+                  <View style={styles.infos}>
+                    <Text style={styles.profileName}>
+                      {card.firstname} , {card.age} ans
+                    </Text>
                     <View style={styles.ratingContainer}>
-                      <Text style={styles.profileNote}>
-                        {card.avis[0].note}/5
-                      </Text>
-                      {Array(5)
-                        .fill()
-                        .map((_, i) =>
-                          i < Math.round(card.avis[0].note) ? (
-                            <Ionicons
-                              key={i}
-                              name="star"
-                              size={23}
-                              color="#ffce0c"
-                            />
-                          ) : (
-                            <Ionicons
-                              key={i}
-                              name="star"
-                              size={23}
-                              color="#444"
-                            />
-                          )
-                        )}
+                      <Text style={styles.profileCity}> {card.city}</Text>
+                      <View style={styles.ratingContainer}>
+                        <Text style={styles.profileNote}>
+                          {card.avis[0].note}/5
+                        </Text>
+                        {Array(5)
+                          .fill()
+                          .map((_, i) =>
+                            i < Math.round(card.avis[0].note) ? (
+                              <Ionicons
+                                key={i}
+                                name="star"
+                                size={23}
+                                color="#ffce0c"
+                              />
+                            ) : (
+                              <Ionicons
+                                key={i}
+                                name="star"
+                                size={23}
+                                color="#444"
+                              />
+                            )
+                          )}
+                      </View>
                     </View>
+                    <Text style={styles.bioLabel}>Bio</Text>
+                    <Text style={styles.bioText}>{card.bio}</Text>
                   </View>
-                  <Text style={styles.bioLabel}>Bio</Text>
-                  <Text style={styles.bioText}>{card.bio}</Text>
                 </View>
-              </View>
-            );
-          }}
-          verticalSwipe={false}
-          onSwipedLeft={handleDislike}
-          onSwipedRight={handleLike}
-          cardIndex={count} // index du profil actuellement visible
-          backgroundColor={'transparent'}
-          stackSize={4} // Nombre de cartes empilées en arrière-plan
-        />
-      </View>
+              );
+            }}
+            verticalSwipe={false}
+            onSwipedLeft={handleDislike}
+            onSwipedRight={handleLike}
+            cardIndex={count} // index du profil actuellement visible
+            backgroundColor={'transparent'}
+            stackSize={4} // Nombre de cartes empilées en arrière-plan
+          />
+        </View>
+          <View style={styles.actionButtons}>
+            <TouchableOpacity
+              style={styles.buttonSwipe}
+              activeOpacity={0.7}
+              onPress={handleSwipeLeft}>
+              <Ionicons name="close-outline" size={65} color="#f74c4f" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.buttonSwipe}
+              activeOpacity={0.7}
+              onPress={handleSwipeRight}>
+              <Ionicons name="heart" size={50} color="#89c1a5" />
+            </TouchableOpacity>
+          </View>
       </View>
     </SafeAreaView>
   );
@@ -140,7 +151,7 @@ const styles = StyleSheet.create({
   },
   i: {
     flex: 1,
-    backgroundColor: '#d5e8de'
+    backgroundColor: '#fff'
   },
   header: {
     flexDirection: 'row',
@@ -172,18 +183,18 @@ const styles = StyleSheet.create({
     marginHorizontal: 10
   },
   main: {
-    height: height * 0.8,
-    // alignItems: 'center',
-    // justifyContent: 'space-around'
+    height: height * 0.77,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   },
   profileContainer: {
     // Adapter la hauteur à un pourcentage de la hauteur de l'écran
     height: height * 0.7,
     // Adapter la largeur à un pourcentage de la largeur de l'écran
-    width: width * 0.90,
+    width: width * 0.9,
     flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor: '#e8efeb',
+    backgroundColor: '#e1ede7',
     borderRadius: 15,
     shadowColor: '#000000',
     shadowOffset: {
@@ -261,7 +272,6 @@ const styles = StyleSheet.create({
   }
 });
 
-
 /*
 const swiperRef = useRef(null);
 
@@ -280,10 +290,10 @@ const swiperRef = useRef(null);
 <Swiper ref={swiperRef}   />
         
 
- <TouchableOpacity style={styles.button} onPress={handleSwipeLeft}>
-          <Text>Swipe Left</Text>
+ <TouchableOpacity style={styles.buttonSwipe} activeOpacity={0.7} onPress={handleSwipeLeft}>
+          <Ionicons name="close-outline" size={65} color="#f74c4f" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleSwipeRight}>
-          <Text>Swipe Right</Text>
+        <TouchableOpacity style={styles.buttonSwipe} activeOpacity={0.7} onPress={handleSwipeRight}>
+          <Ionicons name="heart" size={50} color="#00f99e" />
         </TouchableOpacity>
 */
