6d138ace58bb55dfbecea5b1dc07c0164081a780
Mael <maeldjedid@gmail.com>
2024-05-28 16:05:05 +0200

    q

---
+++ components/proposal.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export default function Proposal() {
+    
+}
