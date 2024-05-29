861fca63464b723eb899db7c7e5e00a92ccbc711
Your name <Your email>
2024-05-29 10:18:51 +0200

    filtr

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -26,17 +26,9 @@ export default function ProfileCard({ navigation }) {
   
   const dispatch = useDispatch();
   const user = useSelector((state) => state.users.value);
-//   const filter = useSelector((state) => state.filters.value);
+  const filter = useSelector((state) => state.filters.value);
   
-//   const profilDataFilter = () => {
-//     return profilsData.filter(profil => 
-//       profil.age >= filter.ageMinValue &&
-//       profil.age <= filter.ageMaxValue &&
-//       profil.avis[0].note >= filter.noteMin
-//     );
-//   };
-// profilDataFilter()
-// console.log(` profilDataFilter => ${profilDataFilter}`)
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -54,13 +46,23 @@ export default function ProfileCard({ navigation }) {
         setLoading(false);
       }
     };
-    console.log(`user.role => ${user.role}`);
     console.log(`chargement useEffect`);
     fetchData();
   }, []);
 
+  const profilDataFilter = (data) => {
+    
+    return data.filter(profil => 
+      profil.age >= filter.ageMinValue &&
+      profil.age <= filter.ageMaxValue &&
+      profil.avis[0].note >= filter.noteMin
+    );
+  };
+  const filteredData = profilsData ? profilDataFilter(profilsData) : [];
+ console.log(` filteredData => ${filteredData}`)
+
   console.log(`user.token => ${user.token}`);
-  // console.log(`user.token => ${user}`);
+  
 
   const addAlike = () => {
     dispatch(addLike(profilsData[count]._id));
