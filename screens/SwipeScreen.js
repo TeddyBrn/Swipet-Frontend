747b5407c672cefbb7f360be8cce57385cd990ec
Your name <Your email>
2024-05-29 15:39:37 +0200

    aucun profil trouvé

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -26,8 +26,6 @@ export default function ProfileCard({ navigation }) {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.users.value);
   const filter = useSelector((state) => state.filters.value);
-  
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,14 +54,10 @@ export default function ProfileCard({ navigation }) {
       }
     };
     console.log(`chargement useEffect`);
+    console.log(`user.token => ${user.token}`);
     fetchData();
   }, [filter.noteMin,filter.ageMax,filter.ageMin]);
   // console.log('profilsdata' , profilsData)
-
-
-
-  console.log(`user.token => ${user.token}`);
-  
 
   const addAlike = () => {
     dispatch(addLike(profilsData[count]._id));
@@ -188,112 +182,115 @@ export default function ProfileCard({ navigation }) {
           </View>
         </Modal>
         <View style={styles.main}>
-{      profilsData.length ?   <Swiper
-            cards={profilsData} // Les données des profils à swiper
-            ref={swiperRef}
-            renderCard={(card) => {
-            
-              return (
-                <View style={styles.profileContainer}>
-                  <Image
-                    style={styles.profileImage}
-                    source={{ uri: card.url }}
-                  />
-                  <View style={styles.infos}>
-                    <Text style={styles.profileName}>
-                      {card.firstname} , {card.age} ans
-                    </Text>
-                    <Text style={styles.profileCity}>
-                      <Ionicons
-                        name="location-outline"
-                        size={18}
-                        color="#33464d"
-                      />{' '}
-                      {card.city}
-                    </Text>
-                    <View style={styles.ratingContainer}>
+          {profilsData.length ? (
+            <Swiper
+              cards={profilsData} // Les données des profils à swiper
+              ref={swiperRef}
+              renderCard={(card) => {
+              
+                return (
+                  <View style={styles.profileContainer}>
+                    <Image
+                      style={styles.profileImage}
+                      source={{ uri: card.url }}
+                    />
+                    <View style={styles.infos}>
+                      <Text style={styles.profileName}>
+                        {card.firstname} , {card.age} ans
+                      </Text>
+                      <Text style={styles.profileCity}>
+                        <Ionicons
+                          name="location-outline"
+                          size={18}
+                          color="#33464d"
+                        />{' '}
+                        {card.city}
+                      </Text>
                       <View style={styles.ratingContainer}>
-                        <Text style={styles.profileNote}>
-                          {card.avis[0].note}/5
-                        </Text>
-                        {Array(5)
-                          .fill()
-                          .map((_, i) => {
-                            const fullStars = Math.floor(card.avis[0].note); // Partie entière de la note
-                            const fractionalStar = card.avis[0].note % 1; // Fraction de la note
+                        <View style={styles.ratingContainer}>
+                          <Text style={styles.profileNote}>
+                            {card.avis[0].note}/5
+                          </Text>
+                          {Array(5)
+                            .fill()
+                            .map((_, i) => {
+                              const fullStars = Math.floor(card.avis[0].note); // Partie entière de la note
+                              const fractionalStar = card.avis[0].note % 1; // Fraction de la note
 
-                            if (i < fullStars) {
-                              // Étoiles entières
-                              return (
-                                <Ionicons
-                                  key={i}
-                                  name="star"
-                                  size={18}
-                                  color="#ffce0c"
-                                />
-                              );
-                            } else if (i === fullStars && fractionalStar > 0) {
-                              // Étoile partielle
-                              return (
-                                <View
-                                  key={i}
-                                  style={{
-                                    position: 'relative',
-                                    width: 18,
-                                    height: 18
-                                  }}>
+                              if (i < fullStars) {
+                                // Étoiles entières
+                                return (
                                   <Ionicons
+                                    key={i}
                                     name="star"
                                     size={18}
-                                    color="#444"
+                                    color="#ffce0c"
                                   />
+                                );
+                              } else if (i === fullStars && fractionalStar > 0) {
+                                // Étoile partielle
+                                return (
                                   <View
+                                    key={i}
                                     style={{
-                                      position: 'absolute',
-                                      top: 0,
-                                      left: 0,
-                                      width: `${fractionalStar * 100}%`,
-                                      height: '100%',
-                                      overflow: 'hidden'
+                                      position: 'relative',
+                                      width: 18,
+                                      height: 18
                                     }}>
                                     <Ionicons
                                       name="star"
                                       size={18}
-                                      color="#ffce0c"
+                                      color="#444"
                                     />
+                                    <View
+                                      style={{
+                                        position: 'absolute',
+                                        top: 0,
+                                        left: 0,
+                                        width: `${fractionalStar * 100}%`,
+                                        height: '100%',
+                                        overflow: 'hidden'
+                                      }}>
+                                      <Ionicons
+                                        name="star"
+                                        size={18}
+                                        color="#ffce0c"
+                                      />
+                                    </View>
                                   </View>
-                                </View>
-                              );
-                            } else {
-                              // Étoiles vides
-                              return (
-                                <Ionicons
-                                  key={i}
-                                  name="star"
-                                  size={18}
-                                  color="#444"
-                                />
-                              );
-                            }
-                          })}
+                                );
+                              } else {
+                                // Étoiles vides
+                                return (
+                                  <Ionicons
+                                    key={i}
+                                    name="star"
+                                    size={18}
+                                    color="#444"
+                                  />
+                                );
+                              }
+                            })}
+                        </View>
                       </View>
+                      <Text style={styles.bioLabel}>Bio</Text>
+                      <Text style={styles.bioText}>{card.bio}</Text>
                     </View>
-                    <Text style={styles.bioLabel}>Bio</Text>
-                    <Text style={styles.bioText}>{card.bio}</Text>
                   </View>
-                </View>
-              );
-            }}
-            verticalSwipe={false}
-            onSwipedLeft={handleDislike}
-            onSwipedRight={handleLike}
-            cardIndex={count} // index du profil actuellement visible
-            backgroundColor={'transparent'}
-            stackSize={3} // Nombre de cartes empilées en arrière-plan
-            stackScale={3}
-            stackSeparation={16}
-          /> : <Text>aucun profil trouvé</Text>}
-          
+                );
+              }}
+              verticalSwipe={false}
+              onSwipedLeft={handleDislike}
+              onSwipedRight={handleLike}
+              cardIndex={count} // index du profil actuellement visible
+              backgroundColor={'transparent'}
+              stackSize={3} // Nombre de cartes empilées en arrière-plan
+              stackScale={3}
+              stackSeparation={16}
+            />
+          ) : (
+            <Text style={styles.notFound}>aucun profil trouvé</Text>
+          )}
         </View>
         <View style={styles.actionButtons}>
           <TouchableOpacity
@@ -512,5 +509,15 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'Montserrat-Bold',
     color: '#333'
+  },
+  notFound: {
+    width: '100%',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 20,
+    fontFamily: 'Quicksand-SemiBold',
+    color: '#666',
+    marginTop: 150
   }
 });
