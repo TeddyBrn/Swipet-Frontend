ee28d4815eccb4ed6e1127e1c94fc52ed8ebdc00
Teddy <teddy.buran33@gmail.com>
2024-05-29 12:19:43 +0200

    't'

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from 'react';
 import {
   View,
   Text,
@@ -17,39 +17,44 @@ import { addMatch } from '../reducers/matchs';
 import Swiper from 'react-native-deck-swiper';
 import { BACKEND_ADRESS } from '../data/urlData';
 
-
 export default function ProfileCard({ navigation }) {
   const [profilsData, setProfilsData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  
+
   const dispatch = useDispatch();
   const user = useSelector((state) => state.users.value);
-//   const filter = useSelector((state) => state.filters.value);
-  
-//   const profilDataFilter = () => {
-//     return profilsData.filter(profil => 
-//       profil.age >= filter.ageMinValue &&
-//       profil.age <= filter.ageMaxValue &&
-//       profil.avis[0].note >= filter.noteMin
-//     );
-//   };
-// profilDataFilter()
-// console.log(` profilDataFilter => ${profilDataFilter}`)
+  // console.log(newMatch.url)
+  // console.log(newMatch.firstname)
+  // console.log(profilsData[count]._id)
+  // console.log(profilsData[count].firstname)
+  //   const filter = useSelector((state) => state.filters.value);
+
+  //   const profilDataFilter = () => {
+  //     return profilsData.filter(profil =>
+  //       profil.age >= filter.ageMinValue &&
+  //       profil.age <= filter.ageMaxValue &&
+  //       profil.avis[0].note >= filter.noteMin
+  //     );
+  //   };
+  // profilDataFilter()
+  // console.log(` profilDataFilter => ${profilDataFilter}`)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${BACKEND_ADRESS}/profils/swipe/${user.role}`);
+        const response = await fetch(
+          `${BACKEND_ADRESS}/profils/swipe/${user.role}`
+        );
         const data = await response.json();
         if (data.result) {
           setProfilsData(data.data);
         } else {
-          setError("Failed to fetch profiles");
+          setError('Failed to fetch profiles');
         }
       } catch (err) {
-        setError("An error occurred");
+        setError('An error occurred');
       } finally {
         setLoading(false);
       }
@@ -64,16 +69,20 @@ export default function ProfileCard({ navigation }) {
 
   const addAlike = () => {
     dispatch(addLike(profilsData[count]._id));
-    fetch(`${BACKEND_ADRESS}/matchs/like/${user.token}/${profilsData[count]._id}`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-          }).then(response => response.json())
-            .then(data => {
-              if (data.message === 'new match created!') {
-                dispatch(addMatch(user._id, profilsData[count]._id))
-                setIsModalVisible(true)         
-              }
-            });
+    fetch(
+      `${BACKEND_ADRESS}/matchs/like/${user.token}/${profilsData[count]._id}`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === 'new match created!') {
+          dispatch(addMatch(user._id, profilsData[count]._id));
+          setIsModalVisible(true);
+        }
+      });
   };
 
   const [count, setCount] = useState(0);
@@ -123,35 +132,60 @@ export default function ProfileCard({ navigation }) {
       <View style={styles.i}>
         <View style={styles.header}>
           <Image
-            source={require("../assets/miniLogo.png")}
+            source={require('../assets/miniLogo.png')}
             style={styles.logo}
           />
           <View style={styles.headerR}>
             <TouchableOpacity
               style={styles.iconButton}
-              onPress={() => navigation.navigate("Notifications")}
-            >
+              onPress={() => navigation.navigate('Notifications')}>
               <Ionicons name="notifications" size={35} color="#33464d" />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.iconButton}
-              onPress={() => navigation.navigate("Settings")}
-            >
-              <Ionicons name="settings-outline" size={35} color="#33464d" />
+              onPress={() => navigation.navigate('Settings')}>
+              <Ionicons name="settings" size={35} color="#33464d" />
             </TouchableOpacity>
           </View>
         </View>
         <Modal
           visible={isModalVisible}
           transparent={true}
-          animationType="slide"
-        >
+          animationType="slide">
           <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
-              <TouchableOpacity onPress={() => setIsModalVisible(false)} style={styles.closeButton}>
-                <Text style={styles.closeButtonText}>Fermer</Text>
-              </TouchableOpacity>
-              <Text style={styles.modalText}>Vous avez un nouveau match!</Text>
+              <View style={styles.modalInfosContainer}>
+                <Text style={styles.modalTitle}>Nouveau match !</Text>
+                <Text style={styles.infosMatch}>{isModalVisible && `Vous et ${profilsData[count-1].firstname} avez un match`}
+                  
+                </Text>
+                <View style={styles.imageContainer}>
+                  <Image
+                    style={styles.infosImage}
+                    source={{ uri: user.photo }}
+                    
+                  />
+                  {isModalVisible &&
+                  <Image
+                    style={styles.infosImage}
+                    source={{ uri: profilsData[count-1].url }}
+                  />}
+                </View>
+              </View>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  onPress={() => {setIsModalVisible(false); navigation.navigate('Settings')}}
+                  style={styles.button}>
+                  <Ionicons name="chatbubbles" size={35} color="#333" />
+                  <Text style={styles.buttonText}>Envoyer un message</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => setIsModalVisible(false)}
+                  style={styles.button}>
+                  <Ionicons name="arrow-undo" size={35} color="#333" />
+                  <Text style={styles.buttonText}>Continuer à Swipe</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </Modal>
@@ -175,7 +209,7 @@ export default function ProfileCard({ navigation }) {
                         name="location-outline"
                         size={18}
                         color="#33464d"
-                      />{" "}
+                      />{' '}
                       {card.city}
                     </Text>
                     <View style={styles.ratingContainer}>
@@ -205,11 +239,10 @@ export default function ProfileCard({ navigation }) {
                                 <View
                                   key={i}
                                   style={{
-                                    position: "relative",
+                                    position: 'relative',
                                     width: 18,
-                                    height: 18,
-                                  }}
-                                >
+                                    height: 18
+                                  }}>
                                   <Ionicons
                                     name="star"
                                     size={18}
@@ -217,14 +250,13 @@ export default function ProfileCard({ navigation }) {
                                   />
                                   <View
                                     style={{
-                                      position: "absolute",
+                                      position: 'absolute',
                                       top: 0,
                                       left: 0,
                                       width: `${fractionalStar * 100}%`,
-                                      height: "100%",
-                                      overflow: "hidden",
-                                    }}
-                                  >
+                                      height: '100%',
+                                      overflow: 'hidden'
+                                    }}>
                                     <Ionicons
                                       name="star"
                                       size={18}
@@ -257,7 +289,7 @@ export default function ProfileCard({ navigation }) {
             onSwipedLeft={handleDislike}
             onSwipedRight={handleLike}
             cardIndex={count} // index du profil actuellement visible
-            backgroundColor={"transparent"}
+            backgroundColor={'transparent'}
             stackSize={3} // Nombre de cartes empilées en arrière-plan
             stackScale={3}
             stackSeparation={16}
@@ -267,15 +299,13 @@ export default function ProfileCard({ navigation }) {
           <TouchableOpacity
             style={styles.buttonSwipe}
             activeOpacity={0.7}
-            onPress={handleSwipeLeft}
-          >
+            onPress={handleSwipeLeft}>
             <Ionicons name="close-outline" size={55} color="#f74c4f" />
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.buttonSwipe}
             activeOpacity={0.7}
-            onPress={handleSwipeRight}
-          >
+            onPress={handleSwipeRight}>
             <Ionicons name="heart" size={40} color="#8ad1ad" />
           </TouchableOpacity>
         </View>
@@ -285,171 +315,202 @@ export default function ProfileCard({ navigation }) {
 }
 
 // Récupère les dimensions de l'écran
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   i: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff'
   },
   header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    shadowColor: "#000000",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    shadowColor: '#000000',
     shadowOffset: {
       width: 0,
-      height: 20,
+      height: 20
     },
     shadowOpacity: 0.25,
     shadowRadius: 20.0,
-    elevation: 20,
+    elevation: 20
   },
   headerR: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10
   },
   logo: {
     width: 60,
     height: 60,
-    contentFit: "contain", //"resizeMode" is deprecated, use "contentFit" instead
+    contentFit: 'contain', //"resizeMode" is deprecated, use "contentFit" instead
     marginLeft: 20,
-    marginVertical: 5,
+    marginVertical: 5
   },
   iconButton: {
-    marginHorizontal: 10,
+    marginHorizontal: 10
   },
   main: {
     height: height * 0.71,
-    alignItems: "center",
-    justifyContent: "flex-start",
-    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'column'
   },
   profileContainer: {
     // Adapter la hauteur à un pourcentage de la hauteur de l'écran
     height: height * 0.64,
     // Adapter la largeur à un pourcentage de la largeur de l'écran
     width: width * 0.9,
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: "#e1ede7",
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#e1ede7',
     borderRadius: 15,
-    shadowColor: "#000000",
+    shadowColor: '#000000',
     shadowOffset: {
       width: 0,
-      height: 18,
+      height: 18
     },
     shadowOpacity: 0.25,
     shadowRadius: 20.0,
     elevation: 10,
-    marginTop: -45,
+    marginTop: -45
   },
   profileImage: {
     // Adapter la hauteur et la largeur à un pourcentage de la hauteur de l'écran
-    width: "100%",
-    height: height * 0.30,
+    width: '100%',
+    height: height * 0.3,
     borderTopEndRadius: 15,
     borderTopStartRadius: 15,
-    borderRadius: 3,
+    borderRadius: 3
   },
   infos: {
-    width: "90%",
-    marginTop: 5,
+    width: '90%',
+    marginTop: 5
   },
   profileName: {
     fontSize: 26,
-    fontFamily: "Quicksand-Bold",
+    fontFamily: 'Quicksand-Bold'
   },
   profileCity: {
     fontSize: 18,
-    fontFamily: "Quicksand-SemiBold",
-    color: "#666",
+    fontFamily: 'Quicksand-SemiBold',
+    color: '#666'
   },
   ratingContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   profileNote: {
     fontSize: 18,
-    fontFamily: "Quicksand-SemiBold",
-    color: "#666",
-    paddingHorizontal: 10,
+    fontFamily: 'Quicksand-SemiBold',
+    color: '#666',
+    paddingHorizontal: 10
   },
   bioLabel: {
     fontSize: 22,
-    color: "#222",
-    fontFamily: "Quicksand-Bold",
+    color: '#222',
+    fontFamily: 'Quicksand-Bold',
     marginTop: 20,
-    marginBottom: 5,
+    marginBottom: 5
   },
   bioText: {
     fontSize: 18,
-    fontFamily: "Quicksand-SemiBold",
-    color: "#666",
+    fontFamily: 'Quicksand-SemiBold',
+    color: '#666'
   },
   actionButtons: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
   },
   buttonSwipe: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 50,
     height: 60,
     width: 60,
-    alignItems: "center",
-    justifyContent: "center",
-    shadowColor: "#333",
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#333',
     shadowOffset: {
       width: 0,
-      height: 18,
+      height: 18
     },
     shadowOpacity: 0.15,
     shadowRadius: 20.0,
-    elevation: 40,
+    elevation: 40
   },
   modalContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
   },
   modalContent: {
-    backgroundColor: "#e1ede7",
+    backgroundColor: '#fff',
     borderRadius: 10,
     padding: 10,
-    alignItems: "center",
-    justifyContent: "center",
-      height: height * 0.3,
-      width: width * 0.9,
+    alignItems: 'center',
+    // justifyContent: 'flex-start',
+    height: height * 0.6,
+    width: width * 0.9,
+    flexDirection: 'column'
   },
-  closeButton: {
-    alignSelf: "flex-end",
-    // borderColor: "#000000",
-    // borderWidth: 1.5,
-    height: height * 0.1,
-    width: width * 0.4,
-    marginTop: -90,
-    
+  modalInfosContainer: {
+    width: width * 0.9,
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: height * 0.5
   },
-  closeButtonText: {
+  modalTitle: {
+    fontSize: 40,
+    color: '#72e2ba',
+    fontFamily: 'PoetsenOne-Regular',
+    paddingVertical: 20
+  },
+  infosMatch: {
     fontSize: 20,
-    color: "#f74c4f",
-    borderBottomWidth: 1.5,
-    borderBottomColor: "#f74c4f",
-    alignSelf: "center",
-    
+    color: '#333',
+    fontFamily: 'PoetsenOne-Regular',
+    paddingVertical: 10
   },
-  modalText: {
+  imageContainer: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    alignItems: 'center',
+    width: width * 0.8,
+    justifyContent: 'space-around',
+    paddingVertical: 20
+  },
+  infosImage: {
+    width: 120,
+    height: 120,
+    borderWidth: 4,
+    borderRadius: 100,
+    borderColor: '#333'
+  },
+  buttonContainer: {
+    width: width * 0.7,
+    margin: -100
+  },
+  button: {
+    height: height * 0.07,
+    width: width * 0.7,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#555',
+    marginBottom: 15
+  },
+  buttonText: {
     fontSize: 20,
-    color: "#333",
-
-  },
-
+    fontFamily: 'Montserrat-Bold',
+    color: '#333'
+  }
 });
